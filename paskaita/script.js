d4fb0f1d0e732df8700d98654b2adfed9b7bb729
username <fruttys@gmail.com>
2022-04-26 20:11:32 +0300

    add let

---
+++ paskaita/script.js
@@ -0,0 +1,13 @@
+// comment vienos linijos
+/*
+multiline command 
+daugialinijinis komentaras
+*/
+
+// sukuriam kintamuosius
+let myName; 
+let age;
+
+// priskiriam kintamiesiems reiksmes
+myName = "Liudmila";
+age = 19;
